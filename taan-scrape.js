a439eef048d19a5b7a89cc171f9053dc7c29f4dc
Paras <nischalkc21@hotmail.com>
2019-09-12 23:36:25 +0545

    2nd commit

---
+++ taan-scrape.js
@@ -0,0 +1,68 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const writeStream = fs.createWriteStream("agents.txt");
+
+const alphabets = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+let alphArrLength = 0;
+
+let currentVariable = alphabets[alphArrLength];
+
+const getAgentsforAlphabet = number => {
+  let alphabet = alphabets[number];
+  request(
+    `https://www.taan.org.np/pages/general-members/${alphabet}`,
+    (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        $(".member-detail-link").each((i, el) => {
+          const name = $(el)
+            .find("a")
+            .text();
+          console.log(name);
+          writeStream.write(`${name} \n`);
+        });
+        console.log("Scraping Done...", alphabet);
+      }
+      alphArrLength = alphArrLength + 1;
+      console.log(alphArrLength);
+      console.log(currentVariable, "currentVariable");
+      if (alphArrLength != 26) {
+        getAgentsforAlphabet(alphArrLength);
+      } else {
+        return;
+      }
+    }
+  );
+};
+
+getAgentsforAlphabet(alphArrLength);
